a87d062df98321fee76e042814a40b43c0353efe
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-06-29 00:35:16 -0700

    vault functionality (binary file read/write)

---
+++ vault.py
@@ -0,0 +1,29 @@
+from pickle import *
+
+def storeInVault(users):
+    with open('pwd.vault', 'ab') as file:
+        dump(users, file)
+        
+def readVault():
+    with open('pwd.vault', 'rb') as file:
+        
+        while True:
+            try:
+                loadNames = load(file)
+                print(loadNames)
+            except Exception as e:
+                print('No more names')
+                break
+                
+        
+def checkVault():
+    try:
+        with open('pwd.vault', 'rb') as file:
+            load(file)
+        return True
+        
+    except Exception as e:
+        print("Exception: ", e)
+        storeInVault([])
+        return False
+        
