6cf4a8772bcfde9d85a895f4c5ad90d410cf2cf1
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-06-29 00:34:46 -0700

    updated loop functionality in main

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
-def main():
-        
+from vault import *
+
+def main(master):
+    
     flag = True
 
     while(flag):    
         try:
+            if not checkVault():
+                print('Vault was missing, created another one! ')
+            
             x = input("Enter master password: ")
 
             #Check if file exists. If not, create Master Password, else ask user for it
             if(x == master):
-                print("Hello! ")
-            
+                flag2 = True
+                
+                while(flag2):
+                    print("Hello! What do you want to do today? ")                    
+                    y = input("1 to read, 2 to write, 3 to quit: ")
+                    
+                    if(y == '1'):
+                        readVault()
+                        print('')
+                    
+                    elif(y == '2'):
+                        t = ''
+                        users = []
+                        
+                        while t != '0':
+                            t = input("Enter a service, username, password, pressing enter after each. Enter 0 when you're done. ")
+                            if t != '0':
+                                users.append(t)
+                            print(users)
+                            print()
+                            
+                        storeInVault(users)
+                    
+                    elif(y == '3'):
+                        break
+                    
+                    else:
+                        print('Error. Not an option' )
+                        print()
+                
+                flag = False
             
             else:
-                print("Wrong password")
+                print("Wrong password ")
         
         except Exception as e:
-            print("Error")
+            break
 
 
 if __name__ == '__main__':
-    
-    main()
+    master = "kavish"
+    main(master)
     print("Bye!")
