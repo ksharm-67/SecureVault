22e7190eda6fb97beadaef176646c33696cf29b9
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-06-30 02:53:06 -0700

    Added master checking to main

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from vault import *
+from crypto import * 
 
-def main(master):
+def main():
     
     flag = True
 
     while(flag):    
         try:
             if not checkVault():
+                newPass = input('Setup your master password: ')
+                masterContents = firstSetup(newPass)
+                pwdDump(masterContents)
+                
                 print('Vault was missing, created another one! ')
+                storeInVault([])
+                
+            else:
+                masterContents = loadPwd()
             
-            x = input("Enter master password: ")
+            entered = input("Enter master password or enter 0 to exit: ")
 
             #Check if file exists. If not, create Master Password, else ask user for it
-            if(x == master):
+            if(verifyPass(entered, bytes.fromhex(masterContents[0]), bytes.fromhex(masterContents[1]), masterContents[2])):
                 flag2 = True
                 
                 while(flag2):
                     print("Hello! What do you want to do today? ")                    
-                    y = input("1 to read, 2 to write, 3 to quit: ")
+                    y = input("1 to read, 2 to write, 3 to log out, 4 to check password contents: ")
                     
                     if(y == '1'):
                         readVault()
@@ -42,22 +51,27 @@ def main(master):
                             storeInVault(users)
                     
                     elif(y == '3'):
-                        break
+                        flag2 = False
+                        
+                    elif(y == '4'):
+                        loadPwd()
                     
                     else:
                         print('Error. Not an option' )
                         print()
-                
-                flag = False
+                        
+            elif(entered == '0'):
+                break
             
             else:
-                print("Wrong password ")
-        
+                print('Wrong password')
+                    
         except Exception as e:
+            print('Exception: ', e)
             break
 
 
 if __name__ == '__main__':
-    master = "kavish"
-    main(master)
+    #master = "kavish"
+    main()
     print("Bye!")
