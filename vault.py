40081a33654e024a804b8f9a6759c94e7f573aa7
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-07-01 01:28:59 -0700

    Added delete functionality

--- vault.py
+++ vault.py
@@ -1,10 +1,45 @@
 from pickle import *
+import os
 
 def storeInVault(users: list):
     with open('storage.vault', 'ab') as file:
         dump(users, file)
         
-def readVault():
+def createVault():
+    with open('storage.vault', 'wb'):
+        pass
+
+def readVault(service: str):
+    matches = []
+    
+    with open('storage.vault', 'rb') as file:
+        while True:
+            try:
+                entry = load(file)
+                if entry[0] == service:
+                    matches.append((entry[1], entry[2]))
+                
+            except Exception as e:
+                break
+    
+    return matches
+
+def delRecord(rec: str):
+    
+    with open('temp.vault', 'wb') as temp:
+        with open('storage.vault', 'rb') as file:
+            while True:
+                try:
+                    entry = load(file)
+                    if entry[1] != rec:
+                        dump(entry, temp)
+                    
+                except Exception as e:
+                    break
+    
+    os.replace('temp.vault', 'storage.vault')
+    
+def viewAll():
     with open('storage.vault', 'rb') as file:
         
         while True:
@@ -12,9 +47,8 @@ def readVault():
                 loadNames = load(file)
                 print(loadNames)
             except Exception as e:
-                print('No more names')
                 break
-                
+            
         
 def checkVault():
     try:
